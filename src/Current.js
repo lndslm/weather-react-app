6eec8976e6e6635233cbdbfde60aeddd82a6d40b
lndslm <lindasalim.ca@gmail.com>
2023-08-21 09:11:46 -0400

    Reorganized the code (useState and API calls)

--- src/Current.js
+++ src/Current.js
@@ -8,92 +8,97 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Current() {
-  let [city, setCity] = useState("");
+  let [weatherData, setWeatherData] = useState({ ready: false });
+  let iconImage = `https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = `86a9ba8a3f6c64c4f7dcbf5d435a1c77`;
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiURL).then(displayWeatherData);
+    setReady(true);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  let [temperature, setTemperature] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  let [wind, setWind] = useState(null);
-  let [description, setDescription] = useState(null);
-  let [icon, setIcon] = useState(null);
-  let iconImage = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-
   function displayWeatherData(response) {
-    setTemperature(response.data.main.temp);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-    setDescription(response.data.weather[0].description);
-    setIcon(response.data.weather[0].icon);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: "Friday 12:12PM",
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+    });
   }
 
-  return (
-    <div className="Current">
-      <form onSubmit={handleSubmit}>
-        <div className="search-engine">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-10">
-                <input
-                  type="text"
-                  name="city"
-                  className="city-name"
-                  placeholder="Enter city name"
-                  autoComplete="off"
-                  onChange={updateCity}
-                />
-              </div>
-              <div className="col-2">
-                <button type="button" className="btn">
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
-                <button type="button" className="btn">
-                  <FontAwesomeIcon icon={faLocationDot} />
-                </button>
+  if (weatherData.ready) {
+    return (
+      <div className="Current">
+        <form onSubmit={handleSubmit}>
+          <div className="search-engine">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-10">
+                  <input
+                    type="text"
+                    name="city"
+                    className="city-name"
+                    placeholder="Enter city name"
+                    autoComplete="off"
+                    onChange={updateCity}
+                  />
+                </div>
+                <div className="col-2">
+                  <button type="button" className="btn">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </button>
+                  <button type="button" className="btn">
+                    <FontAwesomeIcon icon={faLocationDot} />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
-      <Card className="card current-datas">
-        <Card.Body className="card-body">
-          {" "}
-          <Row className="row">
-            <Col className="col-8">
-              <h1>{city}</h1>
-              <p className="date">Friday 12:12 PM</p>
-              <h2>
-                <img src={iconImage} alt={description} />
-                <span className="sky">{description}</span>
-              </h2>
-            </Col>
-            <Col className="col-4">
-              <h3>
-                <span className="current-temperature">
-                  {Math.round(temperature)}
-                </span>
-                <span className="units">
-                  <small>°C | F</small>
-                </span>
-              </h3>
-              <p className="weather-datas">
-                wind : {wind} km/h
-                <br />
-                humidity : {humidity}%
-              </p>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    </div>
-  );
+        </form>
+        <Card className="card current-datas">
+          <Card.Body className="card-body">
+            {" "}
+            <Row className="row">
+              <Col className="col-8">
+                <h1>{weatherData.city}</h1>
+                <p className="date">{weatherData.date}</p>
+                <h2>
+                  <img src={iconImage} alt={weatherData.description} />
+                  <span className="sky">{weatherData.description}</span>
+                </h2>
+              </Col>
+              <Col className="col-4">
+                <h3>
+                  <span className="current-temperature">
+                    {Math.round(weatherData.temperature)}
+                  </span>
+                  <span className="units">
+                    <small>°C | F</small>
+                  </span>
+                </h3>
+                <p className="weather-datas">
+                  wind : {weatherData.wind} km/h
+                  <br />
+                  humidity : {weatherData.humidity}%
+                </p>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  } else {
+    let apiKey = `86a9ba8a3f6c64c4f7dcbf5d435a1c77`;
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${weatherData.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayWeatherData);
+
+    return "loading...";
+  }
 }
