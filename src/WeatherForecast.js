9bb4699fefdb1179fca2727e5e5de35e9c67b120
lndslm <lindasalim.ca@gmail.com>
2023-08-29 09:21:52 -0400

    Changed the apiKey + apiUrl and created the WeatherForecastDay.js component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
@@ -22,18 +22,7 @@ export default function WeatherForecast(props) {
           <Card.Body className="card-body">
             <Row className="row">
               <Col className="col">
-                <div className="weather-forecast-date">{forecast[0].dt}</div>
-                <div className="weather-forecast-icon">
-                  <WeatherIcon code={forecast[0].weather[0].icon} size={36} />
-                </div>
-                <div className="weather-forecast-temperature">
-                  <span className="weather-forecast-temperature-max">
-                    {forecast[0].temp.max}°
-                  </span>
-                  <span className="weather-forecast-temperature-min">
-                    {forecast[0].temp.min}°
-                  </span>
-                </div>
+                <WeatherForecastDay data={forecast[0]} />
               </Col>
             </Row>
           </Card.Body>
@@ -44,8 +33,8 @@ export default function WeatherForecast(props) {
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
     let units = "metric";
-    let apiKey = "86a9ba8a3f6c64c4f7dcbf5d435a1c77";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={minutely,hourly}&appid=${apiKey}&units=${units}`;
+    let apiKey = "515c9ddbeb3cda9061acfab71031839e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,40 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="weather-forecast-date">{day()}</div>
+      <div className="weather-forecast-icon">
+        <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      </div>
+      <div className="weather-forecast-temperature">
+        <span className="weather-forecast-temperature-max">
+          {maxTemperature()}°
+        </span>
+        <span className="weather-forecast-temperature-min">
+          {minTemperature()}°
+        </span>
+      </div>
+    </div>
+  );
+}
